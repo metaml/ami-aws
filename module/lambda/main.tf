fc565d4d85c417d183e11007f8c6a8b959483623
metaml <metaml@gmail.com>
2023-09-08 02:42:36 -0400

    debugging aws iam policy

--- module/lambda/main.tf
+++ module/lambda/main.tf
@@ -24,11 +24,9 @@ resource "aws_iam_policy" "babel" {
   policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
-       {
-         "Effect": "Allow",
+      {
+        "Effect": "Allow",
         "Actions": [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
           "logs:PutLogEvents"
         ],
         "Resource": "arn:aws:logs:*:*:*"
