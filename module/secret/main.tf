2f5b68701f475db60e13188873eb01958e16be54
metaml <metaml@gmail.com>
2023-09-27 18:05:54 -0400

    added description to secrets

--- module/secret/main.tf
+++ module/secret/main.tf
@@ -1,9 +1,11 @@
 resource "aws_secretsmanager_secret" "babel-github-token" {
   name = "github-token"
+  description = "babel access token to github/Karmanplus"
 }
 
 resource "aws_secretsmanager_secret" "babel-slack-token" {
   name = "slack-token"
+  description = "babel token for slack/Karman+"
 }
 
 output "babel-github-token" {
