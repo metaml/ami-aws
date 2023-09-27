31461ac0e3e23f9c1dbb979e7890b25a84014b74
metaml <metaml@gmail.com>
2023-09-27 16:48:39 -0400

    added github api token

--- module/secret/main.tf
+++ module/secret/main.tf
@@ -1,7 +1,15 @@
-resource "aws_secretsmanager_secret" "babel" {
+resource "aws_secretsmanager_secret" "babel-slack" {
   name = "slack-api-token"
 }
 
+resource "aws_secretsmanager_secret" "babel-github" {
+  name = "github-api-token"
+}
+
 output "arn" {
-  value = "${aws_secretsmanager_secret.babel.arn}"
+  value = "${aws_secretsmanager_secret.babel-slack.arn}"
+}
+
+output "arn" {
+  value = "${aws_secretsmanager_secret.babel-github.arn}"
 }
