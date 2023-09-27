03502463a33f87a8f691f0c795f4428580906c11
metaml <metaml@gmail.com>
2023-09-27 17:18:58 -0400

    renamed output in secrets module

--- module/secret/main.tf
+++ module/secret/main.tf
@@ -6,10 +6,10 @@ resource "aws_secretsmanager_secret" "babel-github" {
   name = "github-api-token"
 }
 
-output "arn" {
+output "arn_slack" {
   value = "${aws_secretsmanager_secret.babel-slack.arn}"
 }
 
-output "arn" {
+output "arn_github" {
   value = "${aws_secretsmanager_secret.babel-github.arn}"
 }
