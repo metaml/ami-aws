fa05a122917df2e884650ad82c53e9674e68e045
metaml <metaml@gmail.com>
2024-07-30 22:02:24 -0700

    added more secrets

--- module/secret/main.tf
+++ module/secret/main.tf
@@ -1,4 +1,14 @@
-resource "aws_secretsmanager_secret" "openai-api-key-aip" {
+resource "aws_secretsmanager_secret" "openai-api-key" {
   name = "openai-api-key"
   description = "openai api key for AIP"
+}
+
+resource "aws_secretsmanager_secret" "db-user" {
+  name = "db-user"
+  description = "db user"
+}
+
+resource "aws_secretsmanager_secret" "db-password" {
+  name = "db-password"
+  description = "db password"
 }
