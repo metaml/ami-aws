49edc66f27d4077e0a41fcc97c489ddbf9730021
metaml <metaml@gmail.com>
2024-09-05 19:03:39 -0700

    open ports 22 and 443

--- module/ec2/configuration.nix.sh
+++ module/ec2/configuration.nix.sh
@@ -5,7 +5,18 @@ mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix-
 cat > /etc/nixos/configuration.nix <<EOF
 { modulesPath, pkgs, ... }: {
   imports = [ "\${modulesPath}/virtualisation/amazon-image.nix" ];
+
   nix.settings.experimental-features = ["nix-command" "flakes"];
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 22 443 ];
+    allowedUDPPortRanges = [
+      { from = 4000; to = 4007; }
+      { from = 8000; to = 8010; }
+    ];
+  };
+  system.stateVersion = "24.05";
+
   environment.systemPackages = with pkgs; [
     git
     awscli2
@@ -31,7 +42,6 @@ cat > /etc/nixos/configuration.nix <<EOF
     zip
     zlib.dev    
   ];
-  system.stateVersion = "24.05";
 }
 EOF
 
