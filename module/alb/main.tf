3bfd379a281ba16bbe8d4d152d41b68fc728e269
metaml <metaml@gmail.com>
2024-09-14 12:54:43 -0700

    added local cidr for ingress

--- module/alb/main.tf
+++ module/alb/main.tf
@@ -16,13 +16,13 @@ resource "aws_security_group" "http" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [ "50.68.120.205/32", data.aws_vpc.default.cidr_block ]
   }
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
 
